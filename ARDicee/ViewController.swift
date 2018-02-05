aaf4333b9f8ebb5f250f0a65f935e17450287afc
Erik Uecke <erikuecke@gmail.com>
2018-02-05 16:24:52 -0500

    Set up plane node touch.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -77,6 +77,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchedLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchedLocation, types: [.existingPlaneUsingExtent])
+            
+            if !results.isEmpty {
+                print("touched the plane")
+            } else {
+                print("toucheds somewhere else ")
+            }
+        }
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
         if anchor is ARPlaneAnchor {
